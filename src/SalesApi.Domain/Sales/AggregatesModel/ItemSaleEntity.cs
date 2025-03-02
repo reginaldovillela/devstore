d7a0a615982c1bc8a60b4f1e58552acc9cd47bcf
Reginaldo Villela <43396103+reginaldovillela@users.noreply.github.com>
2025-03-02 12:23:39 -0300

    feat(sales): creating sales domain

---
+++ src/SalesApi.Domain/Sales/AggregatesModel/ItemSaleEntity.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SalesApi.Domain.Sales.AggregatesModel;
+
+[Table("items_sales")]
+public class ItemSaleEntity
+    : Entity
+{
+    public Guid ProductId { get; init; }
+
+    public ushort Quantity { get; init; }
+
+    public decimal UnitPrice { get; init; }
+
+    public decimal Discount { get; init; }
+
+    public bool IsCancelled { get; init; }
+
+    #region "ef requirements and relations"
+
+    [ForeignKey("Sale")]
+    [Required]
+    public Guid SaleId { get; private init; }
+
+    public SaleEntity Sale { get; private set; } = null!;
+
+#pragma warning disable CS8618
+    protected ItemSaleEntity() { }
+#pragma warning restore CS8618
+
+    #endregion
+
+}
+
---
+++ src/SalesApi.Infrastructure/Repositories/SalesRepository.cs
@@ -0,0 +1,25 @@
+﻿using SalesApi.Domain.Sales.AggregatesModel;
+using SalesApi.Domain.SeedWork.Interfaces;
+
+namespace SalesApi.Infrastructure.Repositories;
+
+public class SalesRepository : ISalesRepository
+{
+    public IUnitOfWork UnitOfWork => throw new NotImplementedException();
+
+    public Task<bool> DeleteByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ICollection<SaleEntity>> GetAllAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<SaleEntity?> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
+
