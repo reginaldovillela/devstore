ee9c7c4ff15ebc0fe1f36e19cc075c0822d9c7d9
Reginaldo Villela <43396103+reginaldovillela@users.noreply.github.com>
2025-03-04 22:45:14 -0300

    chore(tests): adding first tests

---
+++ tests/SalesApi.Tests/Domain/Products/ProductEntityTests.cs
@@ -0,0 +1,27 @@
+﻿using SalesApi.Domain.Products.AggregatesModel;
+
+namespace SalesApi.Tests.Domain.Products;
+
+public class ProductEntityTests
+{
+    [Theory(DisplayName = "Try to create a product with invalid values")]
+    [InlineData("title", "description", 0, "category", "image")]
+    [InlineData("title more than 10", "description", 0, "category", "image")]
+    [InlineData("title", "description more than 15", 0, "category", "image")]
+    [InlineData("title", "description", 1, "category", "image")]
+    [Trait("Domain", "Product")]
+    public void TryToCreateProductWithInvalidValues_Failure(string title, string description, decimal price, string category, string image)
+    {
+        Assert.Throws<InvalidOperationException>(() => new ProductEntity(title, description, price, category, image));
+    }
+
+    [Theory(DisplayName = "Try to create a product with valid values")]
+    [InlineData("title more than 10", "description more than 15", 1, "category", "image")]
+    [Trait("Domain", "Product")]
+    public void TryToCreateProductWithValidValues_Success(string title, string description, decimal price, string category, string image)
+    {
+        var product = new ProductEntity(title, description, price, category, image);
+
+        Assert.NotNull(product);
+    }
+}
