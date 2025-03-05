c8b7ba28e32bcf4ed39e1d0bddb882da1ebadb5c
Reginaldo Villela <43396103+reginaldovillela@users.noreply.github.com>
2025-03-04 22:45:14 -0300

    chore(tests): adding first tests

---
+++ tests/SalesApi.Tests/Domain/Sales/SaleEntityTestes.cs
@@ -0,0 +1,34 @@
+﻿using SalesApi.Domain.Sales.AggregatesModel;
+
+namespace SalesApi.Tests.Domain.Sales;
+
+public class SaleEntityTestes
+{
+    public static TheoryData<string, DateTime, Guid, Guid> InvalidCases = new()
+    {
+        { "1234", DateTime.Now.AddDays(1), Guid.NewGuid(), Guid.NewGuid() }
+    };
+
+    public static TheoryData<string, DateTime, Guid, Guid> ValidCases = new()
+    {
+        { "1234", DateTime.Now, Guid.NewGuid(), Guid.NewGuid() }
+    };
+
+    [Theory(DisplayName = "Try to create a sale with invalid values")]
+    [MemberData(nameof(InvalidCases))]
+    [Trait("Domain", "Sale")]
+    public void TryToCreateSaleWithInvalidValues_Failure(string saleNumber, DateTime saleDate, Guid customerId, Guid branchId)
+    {
+        Assert.Throws<InvalidOperationException>(() => new SaleEntity(saleNumber, saleDate, customerId, branchId));
+    }
+
+    [Theory(DisplayName = "Try to create a sale with valid values")]
+    [MemberData(nameof(ValidCases))]
+    [Trait("Domain", "Sale")]
+    public void TryToCreateSaleWithValidValues_Success(string saleNumber, DateTime saleDate, Guid customerId, Guid branchId)
+    {
+        var sale = new SaleEntity(saleNumber, saleDate, customerId, branchId);
+
+        Assert.NotNull(sale);
+    }
+}
